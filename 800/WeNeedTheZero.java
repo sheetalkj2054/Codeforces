070aff84ada75c201b0158f89ccb439b9552dddd
sheetalkj2054 <sheetalkj2054@gmail.com>
2025-08-01 02:27:44 +0530

    WeNeedTheZero

--- 800/WeNeedTheZero.java
+++ 800/WeNeedTheZero.java
@@ -5,22 +5,18 @@ public class WeNeedTheZero {
         int t = sc.nextInt();
         while (t > 0) {
             int n = sc.nextInt();
- 
-            int[] a = new int[n];
-            int ans = 0;
-            for (int i = 0; i < n; i++) {
-                a[i] = sc.nextInt();
-                ans = ans^a[i];
-            }
-            if(n%2==1){
-                System.out.println(ans);
-            }else{
-                if (ans == 0) {
-                    System.out.println(0);
-                } else {
-                    System.out.println(-1);
+            String str = sc.next();
+            int s = 0;
+            int e = n-1;
+            while(s<e){
+                if(str.charAt(s)=='0' && str.charAt(e)=='1' || str.charAt(s)=='1' && str.charAt(e)=='0'){
+                    s++;
+                    e--;
+                }else{
+                    break;
                 }
             }
+            System.out.println(e-s+1);
             t--;
         }
 
