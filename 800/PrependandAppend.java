dbfc2978a74a7706ef5823643413ef84ac9079b0
sheetalkj2054 <sheetalkj2054@gmail.com>
2025-08-02 00:05:10 +0530

    Updated Code

--- 800/PrependandAppend.java
+++ 800/PrependandAppend.java
@@ -1,6 +1,25 @@
 import java.util.*;
 public class PrependandAppend {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            int n = sc.nextInt();
+            String str = sc.next();
+            int s = 0;
+            int e = n-1;
+            while(s<e){
+                if(str.charAt(s)=='0' && str.charAt(e)=='1' || str.charAt(s)=='1' && str.charAt(e)=='0'){
+                    s++;
+                    e--;
+                }else{
+                    break;
+                }
+            }
+            System.out.println(e-s+1);
+            t--;
+        }
+
+        sc.close();
     }
 }
--- 800/WeNeedTheZero.java
+++ 800/WeNeedTheZero.java
@@ -5,21 +5,25 @@ public class WeNeedTheZero {
         int t = sc.nextInt();
         while (t > 0) {
             int n = sc.nextInt();
-            String str = sc.next();
-            int s = 0;
-            int e = n-1;
-            while(s<e){
-                if(str.charAt(s)=='0' && str.charAt(e)=='1' || str.charAt(s)=='1' && str.charAt(e)=='0'){
-                    s++;
-                    e--;
-                }else{
-                    break;
+ 
+            int[] a = new int[n];
+            int ans = 0;
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+                ans = ans^a[i];
+            }
+            if(n%2==1){
+                System.out.println(ans);
+            }else{
+                if (ans == 0) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(-1);
                 }
             }
-            System.out.println(e-s+1);
             t--;
         }
-
+ 
         sc.close();
     }
 }
